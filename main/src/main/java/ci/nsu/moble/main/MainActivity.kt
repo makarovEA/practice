60155c0119d712089bbc6e156bb9a564f7b4722e
MakarovEA <makarovea@mer.ci.nsu.ru>
2026-04-23 11:00:25 +0700

    Fix a lot of problems

--- main/src/main/java/ci/nsu/moble/main/MainActivity.kt
+++ main/src/main/java/ci/nsu/moble/main/MainActivity.kt
@@ -7,14 +7,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import ci.nsu.moble.main.ui.counter.CounterScreen
-import ci.nsu.moble.main.ui.main.YourAppTheme // замените на имя вашей темы, если отличается
+import ci.nsu.moble.main.ui.main.CounterScreen
+import ci.nsu.moble.main.ui.main.YourAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            YourAppTheme { // или MaterialTheme, если тема не создана
+            MaterialTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
